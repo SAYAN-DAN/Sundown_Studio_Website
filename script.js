a7d79ec72580da514e178816b7ab80b0d9fb3909
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-02-10 20:23:15 +0530

    Add Some Changes

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ elemC.addEventListener("mouseleave", function () {
   fixed.style.display = "none";
 });
 
+//  WHEN I HAVE ONLY ONE ELEMMENT
+// var elem1 = document.querySelector("#elem1");
+// elem1.addEventListener("mouseenter", function () {
+//   var image = elem1.getAttribute("data-image");
+//   fixed.style.backgroundImage = `url(${image})`; //Template literals
+// });
+
+//WHEN I HAVE MULTIPLE ELEMENT
+
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    var image = e.getAttribute("data-image");
+    fixed.style.backgroundImage = `url(${image})`;
+  });
+});
