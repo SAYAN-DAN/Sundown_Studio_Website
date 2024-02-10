3ffd050f3effa741c599a51edf673cde9213b6ac
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-02-10 11:19:47 +0530

    Add Some Commit

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
+  el: document.querySelector(".main"),
+  smooth: true,
+});
+
+let elemC = document.querySelector(".elem-container");
+let fixed = document.querySelector(".fixed-image");
+elemC.addEventListener("mouseenter", function () {
+  fixed.style.display = "block";
+});
+elemC.addEventListener("mouseleave", function () {
+  fixed.style.display = "none";
 });
